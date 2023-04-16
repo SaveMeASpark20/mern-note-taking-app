9c0888e9b509fe73924a9d2ac6ed4d35f3244b37
christian leguiz <christianleguiz13@gmail.com>
2023-04-16 16:23:40 +0800

    change the baseURL for deployment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import axios from 'axios';
 
-axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://note-taking-app-f31h.onrender.com";
 axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
